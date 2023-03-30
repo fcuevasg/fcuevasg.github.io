ee790194793309459014a24b7286e51a8ea6356b
nachomglz <nachopad3@gmail.com>
2023-03-30 20:34:40 +0200

    Added new interfaces for teams and users

--- src/components/interfaces/Interfaces.tsx
+++ src/components/interfaces/Interfaces.tsx
@@ -8,22 +8,35 @@ export interface IGridBreakpoints {
   [P: string]: number;
 }
 
-export enum DAILYSTATES{
-  none=0,
-  OK=1,
-  NEEDSHELP=2,
-  BLOCKED=3
+export enum DAILYSTATES {
+  none = 0,
+  OK = 1,
+  NEEDSHELP = 2,
+  BLOCKED = 3
 }
 
-export interface DailyData{
-  status:DAILYSTATES,
-  time:number
+export interface DailyData {
+  status: DAILYSTATES,
+  time: number
 }
 
-export interface TeamMember{
-  name:string,
-  email?:string,
-  dailyData:{
+export interface TeamMember {
+  name: string,
+  email?: string,
+  dailyData: {
     [key: number]: DailyData;
   }
+}
+
+export interface User {
+  _id?: {
+    $oid: string
+  },
+  name: string,
+  email: string,
+}
+
+export interface Team {
+  name: string,
+  users: Array<User>
 }
