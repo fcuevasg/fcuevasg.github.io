a91a1bd8281fafea496f00c3a8ba239d7246e933
nachomglz <nachopad3@gmail.com>
2023-04-02 18:10:19 +0200

    Added react router to dependencies

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,20 @@
+import { TeamMember } from "../components/interfaces/Interfaces"
+
 export function getThemeFromLocalStorage(): string {
   return localStorage.getItem("scrumtools-theme") || "dark";
+}
+
+export function getMembersFromLocalStorage(): TeamMember[] {
+  const localStorageMembers = localStorage.getItem("scrumtools-members");
+
+  if (localStorageMembers && localStorageMembers?.length > 0) {
+    return JSON.parse(localStorageMembers) as TeamMember[];
+  }
+
+  return [];
+}
+
+
+export function getSpeakingFromLocalStorage(): string {
+  return localStorage.getItem("scrumtools-speaking") || "0";
 }
